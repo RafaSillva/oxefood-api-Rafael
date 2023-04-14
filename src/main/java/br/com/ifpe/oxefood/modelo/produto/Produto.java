071a249b75686d89251192c17aacc6303c8a2955
aluno <aluno@ifpe.com>
2023-04-14 17:14:07 -0300

    Projeto-Web III

---
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/Produto.java
@@ -0,0 +1,34 @@
+package br.com.ifpe.oxefood.modelo.produto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Where;
+
+import br.com.ifpe.oxefood.util.entity.EntidadeAuditavel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Produto")
+@Where(clause = "habilitado = true")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produto extends EntidadeAuditavel  {
+
+    private static final long serialVersionUID = -9147515922627050356L;
+
+    @Column
+    private String nome_produto;
+
+    @Column
+    private String cod_produto;
+
+}
